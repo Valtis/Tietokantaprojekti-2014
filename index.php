aaf5bea9efc23ca67ff7a99a1b5d58eeaec09a6a
Valtis <erkka_kaaria@outlook.com>
2014-01-13 20:51:38 +0200

    Actually added index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $muuttuja = "Moi";
+// put your code here
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo $muuttuja; ?></title>
+    </head>
+    <body>
+        <?php echo $muuttuja; ?>
+    </body>
+</html>
