bec05759509a609bb95f751cfdf8945456cb7ca7
Valtis <erkka_kaaria@outlook.com>
2014-01-22 23:29:03 +0200

    HTML mockups, updated table creation and deletion.

--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,1 +1,9 @@
+DROP TABLE topic_threads;
+DROP TABLE topics;
+DROP TABLE read_threads;
+DROP TABLE thread_posts;
+DROP TABLE threads;
+DROP TABLE replies;
+DROP TABLE private_messages;
+DROP TABLE posts;
 DROP TABLE users;
