f302ea0045ce43f55c6a8c212ca70ff4e5486427
Valtis <erkka_kaaria@outlook.com>
2014-01-26 21:57:21 +0200

    Reply-button now hidden if user isn't logged in. Changed few file types from html to php.

--- views/threadView.php
+++ views/threadView.php
@@ -6,31 +6,37 @@
         
 <div class="left-margin">
     <h1>Viestit</h1>
-    <div>
+    
+
         <?php foreach ($raw_data['posts'] as $p) { ?>
-        <hr>
-        <a id="<?php echo $p['postdata']->getPostID() ?>"></a>
-        <div>
-            <p><b><a href="private_message.php?userid=<?php echo $p['postdata']->getPosterID() ?>"><?php echo $p['postdata']->getPosterName(); ?></a></b></p>
-            <p><?php echo $p['postdata']->getPostText(); ?></p>
-            <div class="right">
-            <?php if (!empty($p['showdelete'])) { ?>
-                <button type="button" onclick="confirm(1234)"  class="btn btn-default">Poista</button>
-                
-            <?php } if (!empty($p['showedit'])) { ?>
-                <button type="button" onclick="window.open('vastaus.html?edit=1&postid=1234', '_self')" class="btn btn-default">Muokkaa</button>
-                
-            <?php } if (!empty($p['showreply'])) { ?>
-                <button type="button" onclick="window.open('quote.php?postid=<?php echo $p['postdata']->getPostID()?>', '_self')" class="btn btn-default">Quote</button>
-                <?php } ?>
-            </div>
+        <div class='panel panel-default'>
+            <div class='panel-body'>
+                <a id="<?php echo $p['postdata']->getPostID() ?>"></a>
+                <div>
+                    <p><b><a href="private_message.php?userid=<?php echo $p['postdata']->getPosterID() ?>"><?php echo $p['postdata']->getPosterName(); ?></a></b></p>
+                    <p><?php echo $p['postdata']->getPostText(); ?></p>
+                    <div class="right">
+                    <?php if (!empty($p['showdelete'])) { ?>
+                        <button type="button" onclick="confirm(1234)"  class="btn btn-default">Delete</button>
+
+                    <?php } if (!empty($p['showedit'])) { ?>
+                        <button type="button" onclick="window.open('edit.php?postid=<?php echo $p['postdata']->getPostID() ?>', '_self')" class="btn btn-default">Edit</button>
+
+                    <?php } if (!empty($p['showreply'])) { ?>
+                        <button type="button" onclick="window.open('quote.php?postid=<?php echo $p['postdata']->getPostID()?>', '_self')" class="btn btn-default">Quote</button>
+                        <?php } ?>
+                    </div>
+                </div>    
+            </div>    
         </div>
                 
         <?php }?>
-        <hr>
-        
+  
+        <?php if (isLoggedIn()) { ?>
         <div class ="right">
-                <button type="button" onclick="window.open('reply.html?threadid=<?php echo $raw_data['threadid']; ?>', '_self')" class="btn btn-default">Vastaa</button>
+                <button type="button" onclick="window.open('reply.php?threadid=<?php echo $raw_data['threadid']; ?>', '_self')" class="btn btn-default">Reply</button>
                 <br><br>
         </div>
-    </div>
+        <?php } ?>
+
+    
